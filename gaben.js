406e0ce99a057355f646b356c349983c7d01b931
GeekyGamer14 <tj.201@me.com>
2014-01-02 22:05:20 -0800

    more maths

--- gaben.js
+++ gaben.js
@@ -1,6 +1,41 @@
-function gaben(a,b){
+var gaben = {};
+
+gaben.add = function(a,b){
 	var answer;
 	math = a + b;
+	if(math == 3){
+		answer = '2: Episode 1';
+	}else{
+		answer = math;
+	}
+	return answer;
+}
+
+gaben.sub = function(a,b){
+	var answer;
+	math = a - b;
+	if(math == 3){
+		answer = '2: Episode 1';
+	}else{
+		answer = math;
+	}
+	return answer;
+}
+
+gaben.mult = function(a,b){
+	var answer;
+	math = a * b;
+	if(math == 3){
+		answer = '2: Episode 1';
+	}else{
+		answer = math;
+	}
+	return answer;
+}
+
+gaben.div = function(a,b){
+	var answer;
+	math = a / b;
 	if(math == 3){
 		answer = '2: Episode 1';
 	}else{
